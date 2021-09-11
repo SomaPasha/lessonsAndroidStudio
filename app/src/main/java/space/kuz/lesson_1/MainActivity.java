13ebbf71fbde93fe03ceb68c88df22466105ab06
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-09-11 11:38:26 +0500

    Переход из одной к другой активити

--- app/src/main/java/space/kuz/lesson_1/MainActivity.java
+++ app/src/main/java/space/kuz/lesson_1/MainActivity.java
@@ -2,13 +2,27 @@ package space.kuz.lesson_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,Zadania2.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/space/kuz/lesson_1/Zadania2.java
@@ -0,0 +1,14 @@
+package space.kuz.lesson_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Zadania2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zadania2);
+    }
+}
